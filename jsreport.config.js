9bae5edaab088b2801d125a6a7a49c4d812b47ca
pofider <honza.pofider@seznam.cz>
2018-04-19 22:38:02 +0200

    add schema

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,5 +1,25 @@
 module.exports = {
-  'name': 'fs-store-aws-s3-persistence',
-  'main': 'lib/main.js',
-  'dependencies': [ 'templates', 'fs-store' ]
+  name: 'fs-store-aws-s3-persistence',
+  main: 'lib/main.js',
+  dependencies: [ 'templates', 'fs-store' ],
+  optionsSchema: {
+    extensions: {
+      'fs-store-aws-s3-persistence': {
+        accessKeyId: { type: 'string' },
+        secretAccessKey: { type: 'string' },
+        bucket: { type: 'string' },
+        lock: {
+          type: 'object',
+          properties: {
+            queueName: { type: 'string' },
+            region: { type: 'string' },
+            enabled: { type: 'boolean' },
+            attributes: {
+              type: 'object'
+            }
+          }
+        }
+      }
+    }
+  }
 }
